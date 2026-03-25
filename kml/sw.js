ae4ac8c6338324c259817e526e7c45dcfa75584c
accesorioscasa2022-cpu <accesorios.casa2022@gmail.com>
2026-03-25 12:27:46 -0600

    Add files via upload

---
+++ kml/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'inspeccion-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+// Instalación: Descarga y guarda los archivos en el celular
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Intercepción: Si no hay internet, saca los archivos de la memoria
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Devuelve el archivo guardado, o intenta descargarlo si hay internet
+                return response || fetch(event.request);
+            })
+    );
+});
