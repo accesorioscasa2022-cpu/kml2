4d095e6f723132ce1704d3d8c9c54183d5f3d374
accesorioscasa2022-cpu <accesorios.casa2022@gmail.com>
2026-03-26 10:12:04 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'inspeccion-v4';
+const CACHE_NAME = 'inspeccion-v5';
 const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
-    './icon.png'
+    './icon.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
 ];
 
-// Instalación: Descarga y guarda los archivos en el celular
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -16,12 +16,10 @@ self.addEventListener('install', event => {
     );
 });
 
-// Intercepción: Si no hay internet, saca los archivos de la memoria
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Devuelve el archivo guardado, o intenta descargarlo si hay internet
                 return response || fetch(event.request);
             })
     );
